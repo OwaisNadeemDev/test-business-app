af742a7400705b550eaf3ffed55d65da5618ee5b
Owais Nadeem <owais.nadeem@centric.ae>
2023-08-28 01:33:36 +0500

    -added mobile menu

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -30,6 +30,7 @@ export class HeaderComponent {
         path:'/careers'
       },
     ];
+  isMobileMenuOpen: any;
 
     constructor(private router: Router){
       this.router.events.subscribe((event)=>{
@@ -38,5 +39,30 @@ export class HeaderComponent {
         }
       })
     }
+
+    toggleMobileMenu(navigateUrl?:string){
+      const mobileMenu:HTMLElement | null = document.getElementById('mobile-menu');
+      if(!mobileMenu){
+        return;
+      }
+      if(!this.isMobileMenuOpen){
+        this.isMobileMenuOpen = true;
+        mobileMenu.style.transform = 'translate3d(0px, 0px, 0px)';
+      }
+      else{
+        this.isMobileMenuOpen = false;
+        mobileMenu.style.transform = 'translate3d(-100%, 0px, 0px)';
+        if(navigateUrl){
+          setTimeout(()=>{
+            this.navigate(navigateUrl);
+          },500)
+        }
+      }
+    }
+
+    navigate(url:string){
+      this.router.navigateByUrl(url);
+    }
+
 }
 
